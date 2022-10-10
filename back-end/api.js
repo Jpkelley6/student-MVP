22fd67d6a3460ffb9affdf1f3fe191bbb7586a46
Jpkelley6 <jpkelley6@gmail.com>
2022-10-10 13:43:17 -0700

    back end changes

---
+++ back-end/api.js
@@ -0,0 +1,37 @@
+//importing express
+const express = require('express')
+//invoking express
+const app = express()
+//importing pg
+const {Client} =  require('pg')
+//connection string
+const connectionString = 'postgresql://postgres:docker@127.0.0.1:5432/meals'
+//connection string connection 
+const client = new Client({
+    connectionString: connectionString
+})
+//connect the client 
+client.connect()
+//using express.json
+app.use(express.json())
+// importing cors 
+const cors = require('cors')
+//using cors
+app.use(cors())
+// our port
+const PORT = process.env.PORT || 3000
+const {response} = require('express')
+
+
+app.get('/api/meals', (req, res, next) => {
+    return res.headersSent == true ? next() :
+    client.query('SELECT * FROM meals')
+    .then(results => res.send(results.rows))
+})
+
+
+
+//port listening
+app.listen(PORT ,()=> {
+    console.log('Server is running', PORT)
+    })
---
+++ back-end/meals.sql
@@ -0,0 +1,16 @@
+DROP TABLE IF EXISTS meals;
+
+CREATE TABLE meals (
+ meal_id serial ,
+ meal_name varchar(40),
+ meal_type varchar(40), 
+ meal_description text
+);
+
+INSERT INTO meals (meal_name, meal_type, meal_description) VALUES
+('Ribs with corn', 'BBQ', 'Rack of smoked ribs and barbeque sauce. Served with a side of buttered corn on the cobb with seasoning'),
+('Brisket with beans', 'BBQ', '1lb of Texas style smoked brisket. Served with a side of bbq bakes beans'),
+('Chicken with salad', 'BBQ', 'Smoked chicken thigh seasoned with chipotle peppers. Served with a ceasar salad'),
+('Pizza and breadsticks', 'Take-out', 'Pizza with your choice of toppins ordered from the local town pizza spot'),
+('Chicken stir-fry and rice', 'Take-out', 'Chicken stir fry combo with white or fried rice and egg roll from the #1 chinese restaurant in town'),
+('Tikka masala with Garlic Naan bread', 'Take-out', 'Tikka masala sauce with your choice of meat, rice, and a side of garlic naan bread from the local Indian restaurant');
